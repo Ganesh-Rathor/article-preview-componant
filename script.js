f1a82c3b176f5bda773c7384b6247d3b0d53f078
Ganesh-Rathor <97675721+Ganesh-Rathor@users.noreply.github.com>
2022-02-22 02:37:31 -0800

    Upload files

---
+++ script.js
@@ -0,0 +1,14 @@
+let share = document.getElementsByClassName("share")[0];
+share.addEventListener("click",()=>{
+    if (document.getElementsByClassName("active")[0].style.visibility === "visible") {
+        
+        document.getElementsByClassName("active")[0].style.visibility = "hidden";
+        share.classList.toggle("cliked");
+    }
+    else{
+        share.classList.toggle("cliked");
+        document.getElementsByClassName("active")[0].style.visibility = "visible";
+    }
+
+ 
+})
